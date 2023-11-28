c4635d597d4283a3f29233f288228aba44772065
ezJun <30545469+dlwlwns52@users.noreply.github.com>
2023-11-29 00:01:15 +0900

    [Bronze II] Title: 진법 변환, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2745. 진법 변환/진법 변환.py
+++ 백준/Bronze/2745. 진법 변환/진법 변환.py
@@ -1,15 +1,17 @@
-alpha = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-
-num = input().split()
-base = int(num[1])
+alpha = ['0','1', '2', '3', '4', '5', '6', '7', '8', '9','A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 su = 0
+ar = []
+ne = []
+
+num = list(input().split())
+num[1] = int(num[1])
 
 for i in range(len(num[0])):
-    digit = num[0][i]
-    if digit.isdigit():
-        value = int(digit)
-    else:
-        value = alpha.index(digit)
-    su += value * (base ** (len(num[0]) - i - 1))
+    ar.append(num[0][i])
+
+for i in range(len(num[0])):
+    if ar[i] in alpha:
+        ne.append((alpha.index(ar[i])))
+        su += ne[i]*(num[1]**(len(ar)-i-1))
 
 print(su)
