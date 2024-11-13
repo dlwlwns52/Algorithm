eb5bfff123f0f683a1f20a6704c96556892a0710
ezJun <30545469+dlwlwns52@users.noreply.github.com>
2024-11-13 12:35:33 +0900

    [Silver V] Title: 뒤집기, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1439. 뒤집기/뒤집기.py
+++ 백준/Silver/1439. 뒤집기/뒤집기.py
@@ -1,19 +1,22 @@
-# 1. 스택을 사용해보자
-# 2. s를 for문으로 stack에 집어넣는데 인덱스 -1자리와 집어넣는 수와 같다면 집어넣지 않는다
-# 3. 다 집어 넣은후 1과 0의 개수를 센다
-# 4. 만약 길이가 1이면 0을 출력한다.
 s = input()
-stack =[]
-count_0 = 0
-count_1 = 1
 
-for i in s:
-    if not stack or stack[-1] != i:
-        stack.append(i)
-if len(stack) == 1:
-    print(0)
-    
+count_0 = 0
+count_1 = 0
+
+# 0001
+for i in range(len(s)-1):
+  if s[i] != s[i+1]:
+    if s[i] == '0':
+      count_0 += 1
+    else:
+      count_1 += 1
+
+if s[-1] == '0':
+  count_0 += 1
 else:
-    count_0 = stack.count("0")
-    count_1 = stack.count("1")
-    print(min(count_0, count_1))
+  count_1 += 1
+
+print(min(count_0, count_1))
+
+
+
