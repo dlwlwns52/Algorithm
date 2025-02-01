3cf6af21e22a9db3f9864fe00ae8c14fcbc40e8f
ezJun <30545469+dlwlwns52@users.noreply.github.com>
2025-02-01 13:46:23 +0900

    [Bronze II] Title: 블랙잭, Time: 88 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.py
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -1,20 +1,12 @@
-N, M = map(int, input().split())
-array =[]
-sum = 0
-total = 0
-total_sum = []
+n,m = list(map(int, input().split()))
+cd = list(map(int, input().split()))
 
-array = list(map(int, input().split()))
+max_sum = 0
+for i in range(n):
+  for k in range(i+1,n):
+    for j in range(k+1, n):
+      if(m >= (cd[i] + cd[k]+ cd[j]) and max_sum < cd[i] + cd[k]+ cd[j]):
+        max_sum = cd[i] + cd[k]+ cd[j]
 
-
-for i in range(N-2):
-    for k in range(i+1, N-1):
-        for j in range(k+1, N):
-            total = array[i] + array[k] + array[j]
-            if total <= M:
-                total_sum.append(total)
-         
-    
-total_sum = sorted(total_sum)
-print(total_sum[len(total_sum)-1])
+print(max_sum)
 
