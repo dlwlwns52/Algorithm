d10126b4c526df6032efb624ca75c5e86de45ce6
ezJun <30545469+dlwlwns52@users.noreply.github.com>
2025-12-22 20:04:29 +0900

    [level 2] Title: 방문 길이, Time: 1.66 ms, Memory: 9.18 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.py
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.py
@@ -1,43 +1,77 @@
+# 5보다 크면 안되고 -5보다 작으면 안된다.
+# 0. count_list설정
+# 1. [0,0]으로 초기값 설정, 이동할 좌표값 end설정
+# 2. dirs 길이만큼 for문 
+# 3. 'U', 'D', 'R', 'L' 만큼 +1하는데 end가 +5보다 크거나 -5보다 작으면 현재값 유지 
+# 4. 해당 이동한 좌표들 imsi_list = [start, end] 설정
+# 4. count_list에 imsi_list 값이 없다면 answer+=1 하고 append
+# 5. 있다면 생략 
+
+
 def solution(dirs):
     answer = 0
     count_list = []
-    start = [0, 0]
-    end = [0, 0]
-
+    start = [0,0]
+    end = [0,0]
+    
     for i in range(len(dirs)):
+        imsi_list = []
         if dirs[i] == "U":
-            if end[1] == 5:
+            if end[1] == 5:   
                 continue
             else:
                 end[1] += 1
+                path = tuple(sorted([tuple(start), tuple(end)]))
 
+                start[1] += 1
+                if path in count_list:
+                    continue
+                else:
+                    answer +=1 
+                    count_list.append(path)
+                    
         elif dirs[i] == "D":
             if end[1] == -5:
                 continue
             else:
                 end[1] -= 1
-
+                path = tuple(sorted([tuple(start), tuple(end)]))
+    
+                start[1] -=1
+                if path in count_list:
+                    continue
+                else:
+                    answer +=1 
+                    count_list.append(path)
+        
         elif dirs[i] == "R":
             if end[0] == 5:
                 continue
             else:
                 end[0] += 1
+                path = tuple(sorted([tuple(start), tuple(end)]))
 
+                start[0] +=1
+                if path in count_list:
+                    continue
+                else:
+                    answer +=1 
+                    count_list.append(path)
+                    
         elif dirs[i] == "L":
             if end[0] == -5:
                 continue
             else:
                 end[0] -= 1
-
-        # 🔑 핵심 수정 1: 길을 하나의 값으로 표현
-        path = tuple(sorted([tuple(start), tuple(end)]))
-
-        # 🔑 핵심 수정 2: 길 하나 기준으로 비교
-        if path not in count_list:
-            count_list.append(path)
-            answer += 1
-
-        # 🔑 핵심 수정 3: start를 end로 이동
-        start = end.copy()
-
+                path = tuple(sorted([tuple(start), tuple(end)]))
+                start[0] -=1
+                if path in count_list:
+                    continue
+                else:
+                    answer +=1 
+                    count_list.append(path)
+    print(count_list)
     return answer
+
+
+
