ec834fabd1576bf8e410700809948f5018e895e9
ezJun <30545469+dlwlwns52@users.noreply.github.com>
2025-12-28 23:55:18 +0900

    [level 2] Title: 방문 길이, Time: 0.31 ms, Memory: 9.22 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.py
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.py
@@ -12,16 +12,17 @@ def solution(dirs):
             continue
         end = (dx, dy)
 #         [기존 좌표, 다음 좌표], [다음좌표, 기존 좌표] set에 넣기
-        list1 = []
-        list1.append(start)
-        list1.append(end)
-#         set에 리스트는 못 넣는다
-        countSet.add(tuple(list1))
-        list1 = []
-        list1.append(end)
-        list1.append(start)
-        countSet.add(tuple(list1))
- 
+#         list1 = []
+#         list1.append(start)
+#         list1.append(end)
+# #         set에 리스트는 못 넣는다
+#         countSet.add(tuple(list1))
+#         list1 = []
+#         list1.append(end)
+#         list1.append(start)
+#         countSet.add(tuple(list1))
+        countSet.add((start[0],start[1],dx,dy))
+        countSet.add((dx,dy,start[0],start[1]))
         start = end       
         # print(countSet)
 
