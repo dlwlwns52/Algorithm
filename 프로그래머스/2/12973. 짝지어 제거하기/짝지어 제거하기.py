45842a6296c7b38d416d1dfcb3503d2416414fde
ezJun <30545469+dlwlwns52@users.noreply.github.com>
2026-01-04 14:42:01 +0900

    [level 2] Title: 짝지어 제거하기, Time: 94.85 ms, Memory: 11.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.py
@@ -0,0 +1,27 @@
+# for문 길이만큼
+# stack생성 하나씩 넣는다.
+# 만약 스택 마지막하고 현재 넣으려는 것과 같다면 stack pop
+# 스택에 아무것도 없으면 현재거 append
+# 다르다면 append
+# 스택이 남아있으면 1반환 아니라면 0반환
+
+def solution(s):
+    answer = -1
+    stack = []
+    
+    for i in s:
+        # 스택에 아무것도 없으면 현재거 append
+        if not stack:
+            stack.append(i)
+            continue
+#           같다면 빼기
+        if stack[-1] == i:
+            stack.pop()
+        else:
+            stack.append(i)
+    if stack:
+        answer = 0
+    else:
+        answer = 1
+
+    return answer
