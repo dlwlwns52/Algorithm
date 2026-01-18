9f384174aad133f6b699b85009a20c2b2c9d3db3
ezJun <30545469+dlwlwns52@users.noreply.github.com>
2026-01-18 12:44:56 +0900

    [level 1] Title: 폰켓몬, Time: 1.07 ms, Memory: 9.98 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -0,0 +1,29 @@
+# 포켓몬 종류의 개수
+
+# nums의 길이 저장
+#  나누기 2 -> 가져갈 포켓몬 크기
+# nums에 있는 값들 set에 넣기
+# 123 234 23
+# 만약 가져갈 포켓몬 크기가(3) set보다 크면{1,2} 그냥 set 길이 반환
+# 작다면 {1,2,3} 2  -> 가져갈 크기 길이 반환
+# 같다면 둘길이 아무거나 반환
+
+def solution(nums):
+    answer = 0
+    length = len(nums) 
+    pLen = length // 2
+    pSet = set()
+    for i in nums:
+        pSet.add(i)
+        
+#         {1,2}   4
+
+    if len(pSet) < pLen:
+        answer = len(pSet)
+#         {1,2,3,4}   2
+    elif len(pSet) > pLen:
+        answer = pLen
+    else:
+        answer = pLen
+    
+    return answer
