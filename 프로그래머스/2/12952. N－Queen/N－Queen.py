c26684c917a4826103162fd83ea1d1f2aea12a53
ezJun <30545469+dlwlwns52@users.noreply.github.com>
2026-01-27 20:51:11 +0900

    [level 2] Title: N-Queen, Time: 3555.05 ms, Memory: 9.08 MB -BaekjoonHub

--- 프로그래머스/2/12952. N－Queen/N－Queen.py
+++ 프로그래머스/2/12952. N－Queen/N－Queen.py
@@ -1,77 +1,17 @@
-# 행별로 하나씩 -> for 문 n개
-# 열, 대각선 같으면 안됨
-# 대각선은 현재좌표 열-열 행-행이 같으면 대각선인것
-# visited True n개
-# 재귀 -> 좌표 값도 저장 -> 배열 선언
+# 이중 for문 안에서 재귀?
+# visited 된 부분은 바로 스킵
+# 퀸 영역에 있으면 바로 스킵 [0][0]
+# 영역인 부분만 따로 모아놓는다? -> 이걸 true로 하면 visited에서 true로 하면 되는거 아닌가?
+# count=1 둬서 visited[False]인 곳이면 +1하고 visisted true 갱신
 # 
 
 
+
 def solution(n):
-    
     answer = 0
-    
-    visited = [False] * n
-    col = [0] * n
-
-    def back(n, row):
-        nonlocal answer
-        if row == n:
-            answer += 1
-        for i in range(n):
-            if not visited[i] and not cross(i, row, col):
-                col[row] = i
-                visited[i] = True
-                back(n, row+1)
-                visited[i] = False
-  
-    back(n, 0)
-            
+    visited = [[False]*n for _ in range(n)]
+    print(visited)
+    # for i in range(n):
+    #     for k in range(n):
+    #         if 
     return answer
-
-
-def cross(i, row, col):
-    for k in range(row):
-        if abs(row - k) == abs(i- col[k]):
-            return True
-    return False
-
-
-
-
-
-# # 행별로 하나씩 -> for 문 n개
-# # 열, 대각선 같으면 안됨
-# # 대각선은 현재좌표 열-열 행-행이 같으면 대각선인것
-# # visited True n개
-# # 재귀 -> 좌표 값도 저장 -> 배열 선언
-# # 
-
-
-# def solution(n):
-#     answer = 0
-#     visited = [False] * n
-#     navs = []
-    
-    
-#     def back(n, row):
-#         for i in range(n):
-#             if not visited[i]:
-#                 nav = (row, i)
-#                 for i in navs:
-#                     if cross(i, nva):
-#                         visited[i] = True
-#                         back(n, row+1)
-#                         visited[i]= False
-                
-#     back(n, 0)
-            
-#     return answer
-
-
-# def cross(a, b):
-#     if a[0] == b[0] and a[1] == b[1]:
-#         return True
-#     return False
-
-
-
