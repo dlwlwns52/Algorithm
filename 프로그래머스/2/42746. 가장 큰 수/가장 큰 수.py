63d24800a695d8d78fb7658c82d2cded5f43654f
ezJun <30545469+dlwlwns52@users.noreply.github.com>
2026-02-10 20:07:28 +0900

    [level 2] Title: 가장 큰 수, Time: 1277.81 ms, Memory: 26 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -9,25 +9,9 @@
 # 그걸 기존 numbers가 키값으로하고 네자리는 값으로매칭
 # 
 def solution(numbers):
-    arr = []
-    for k, i in enumerate(numbers):
-        arr.append(((str(i)*4)[:4], k))
-    arr = sorted(arr, key = lambda x : x[0], reverse=True)
-    
-    
-    arr_index = []
-    for i in arr:
-        arr_index.append(i[1])
-    
-    arr_answer = []
-    
-    for i in arr_index:
-        arr_answer.append(numbers[i])
-    answer = ''.join(map(str, arr_answer))
-    if answer[0] =="0":
-        answer = "0"
-    return answer
-
+    numbers = list(map(str, numbers))
+    numbers.sort(key=lambda x: x*3, reverse=True)
+    return str(int(''.join(numbers)))
 # [980, 93, 9]
 # 93980
 # 98930]
