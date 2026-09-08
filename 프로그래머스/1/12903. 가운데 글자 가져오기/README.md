e45074c83175d0b1e904c2b941d856c18387b3f4
ezJun <30545469+dlwlwns52@users.noreply.github.com>
2026-09-08 19:58:07 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.01 ms, Memory: 4.64 MB -BaekjoonHub

--- 프로그래머스/1/12903. 가운데 글자 가져오기/README.md
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 가운데 글자 가져오기 - 12903 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12903?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12903) 
 
 ### 성능 요약
 
-메모리: 4.95 MB, 시간: 0.02 ms
+메모리: 4.64 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 09월 08일 15:09:45
+2026년 09월 08일 19:58:03
 
 ### 문제 설명
 
--- 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.cpp
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.cpp
@@ -1,18 +1,16 @@
 #include <string>
 #include <vector>
 #include <iostream>
-
 using namespace std;
 
 string solution(string s) {
-    int len = s.size();
-    cout << len;
+    int size = s.size() / 2;
     string answer = "";
     
-    if (len % 2 == 0){
-        answer = string(1,s[(len/2)-1]) + string(1,s[len/2]);
-    } else {
-        answer = s[len/2];
+    if (s.size() % 2 == 0){
+        answer = s.substr(size-1, 2);
+    } else{
+        answer =s.substr(size, 1);
     }
     return answer;
 }
