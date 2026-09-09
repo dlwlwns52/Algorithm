41d3e617641782fce60a432fc53e14f7b50c5e2a
ezJun <30545469+dlwlwns52@users.noreply.github.com>
2026-09-09 13:32:11 +0900

    [level 0] Title: 뒤에서 5등까지, Time: 0.01 ms, Memory: 4.83 MB -BaekjoonHub

--- 프로그래머스/0/181853. 뒤에서 5등까지/뒤에서 5등까지.cpp
+++ 프로그래머스/0/181853. 뒤에서 5등까지/뒤에서 5등까지.cpp
@@ -1,20 +1,15 @@
 #include <string>
 #include <vector>
-#include <iostream>
 #include <algorithm>
-
 using namespace std;
 
 vector<int> solution(vector<int> num_list) {
     vector<int> answer;
     sort(num_list.begin(), num_list.end());
     
-    for (int i = 0; i < 5; i++){
-        answer.push_back(num_list[i]);
-    }
-    
-    return answer;
+    return {num_list.begin(),num_list.begin()+5};
 }
+
 
 
 // 1. num_list 오름차순 정렬
