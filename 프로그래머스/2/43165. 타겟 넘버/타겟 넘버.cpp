e6f1305bca5eda26924e1a86d5debe318ca2d92a
ezJun <30545469+dlwlwns52@users.noreply.github.com>
2026-09-15 12:11:03 +0900

    [level 2] Title: 타겟 넘버, Time: 28.42 ms, Memory: 4.82 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cpp
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cpp
@@ -1,35 +1,33 @@
 #include <string>
 #include <vector>
-
+#include <iostream>
 using namespace std;
 
-int total = 0;
-int answer = 0;
-int sum = 0;
-vector<int> numbersGlobal;
+int cnt = 0;
+int tar = 0;
 
-void dfs(const vector<int>& numbers, int sum, int index){
-    if (index == numbers.size()) {
-        if(sum == total){
-        answer+=1;
-        return;
-        } else{
-            return;
+void dfs(int sum, int index, vector<int> numbers){
+    if(index == numbers.size()){
+        if(tar == sum){
+            cnt +=1;
         }
+        return;
     }
-        
     
-    dfs(numbers, sum+numbers[index], index+1);
-    dfs(numbers, sum-numbers[index], index+1);
-  
+    dfs(sum+numbers[index], index+1, numbers);
+    dfs(sum-numbers[index], index+1, numbers);
 }
 
 int solution(vector<int> numbers, int target) {
-    total = target;
-    dfs(numbers, sum, 0);
-
-    return answer;
+    tar = target;
+    int sum = 0;
+    int index = 0;
+    dfs(sum,index, numbers);
+    return cnt;
 }
 
-        
-//  dfs에서 현재 
+// dfs
+// 1. 전역 cnt값 및 target설정
+// 2. dfs매개변수 -> sum, index, numbers?
+// 3. if(index == numbers.size()-1)  -> if(target == sum ) cnt +1
+// 4. dfs(sum+numbers[index], index+1, numbers)
