7f1cb96fd65165286186b7a28aaf2a2f2d5bfa21
ezJun <30545469+dlwlwns52@users.noreply.github.com>
2026-09-17 10:47:48 +0900

    [level 1] Title: 행렬의 덧셈, Time: 48.13 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.cpp
+++ 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.cpp
@@ -7,15 +7,16 @@ vector<vector<int>> solution(vector<vector<int>> arr1, vector<vector<int>> arr2)
     vector<vector<int>> answer;
     
     for(int i=0; i<arr1.size(); i++){
-        vector<int> row;
-        for(int k=0; k<arr1[0].size(); k++){
-            row.push_back(arr1[i][k] + arr2[i][k]);
+        vector<int> temp;
+        for(int j=0; j<arr1[0].size(); j++){
+            temp.push_back(arr1[i][j] + arr2[i][j]);
         }
-        answer.push_back(row);
+        answer.push_back(temp);
     }
     return answer;
 }
 
 
-// 행길이만큼 for문
-// gkagkrh sjgrl
+// 1. 이중 for문을 만든다 (arr1,arr2길이만큼)
+//  - 첫번째 for문에서 행마다 넣을 벡터를 만든다
+//  - 두번째 for문에서 더하고 끝나면 행백터를 answer에 넣는다.
