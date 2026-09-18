dafef0ac14e81b72c08409a7bc77d7d130d631c0
ezJun <30545469+dlwlwns52@users.noreply.github.com>
2026-09-18 13:37:34 +0900

    [level 2] Title: 타겟 넘버, Time: 2.22 ms, Memory: 4.76 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/README.md
+++ 프로그래머스/2/43165. 타겟 넘버/README.md
@@ -1,10 +1,10 @@
 # [level 2] 타겟 넘버 - 43165 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165?language=cpp) 
 
 ### 성능 요약
 
-메모리: 4.82 MB, 시간: 28.42 ms
+메모리: 4.76 MB, 시간: 2.22 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 09월 15일 12:11:01
+2026년 09월 18일 13:37:31
 
 ### 문제 설명
 
--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cpp
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cpp
@@ -1,33 +1,36 @@
 #include <string>
 #include <vector>
-#include <iostream>
+
 using namespace std;
 
-int cnt = 0;
-int tar = 0;
+int count = 0;
 
-void dfs(int sum, int index, vector<int> numbers){
+
+void dfs(int index, int sum, vector<int>& numbers, int target){
     if(index == numbers.size()){
-        if(tar == sum){
-            cnt +=1;
+        if(sum == target){
+            count+=1;
         }
         return;
     }
     
-    dfs(sum+numbers[index], index+1, numbers);
-    dfs(sum-numbers[index], index+1, numbers);
+    dfs(index+1, sum-numbers[index], numbers,target);
+    dfs(index+1, sum+numbers[index], numbers,target);
 }
 
 int solution(vector<int> numbers, int target) {
-    tar = target;
-    int sum = 0;
     int index = 0;
-    dfs(sum,index, numbers);
-    return cnt;
+    int sum = 0;
+    
+    dfs(index, sum, numbers, target);
+    
+    return count;
 }
 
-// dfs
-// 1. 전역 cnt값 및 target설정
-// 2. dfs매개변수 -> sum, index, numbers?
-// 3. if(index == numbers.size()-1)  -> if(target == sum ) cnt +1
-// 4. dfs(sum+numbers[index], index+1, numbers)
+
+
+
+// 1. dfs에 sum하고 index넣는다 , count전역변수
+//  - index가 넘버 길이-1랑 같으면 반환하되 sum이 target인경우  count+1
+//  - dfs(sum-numbers[index], index+1), dfs(sum+numbers[index], index+1) 두개 지정
+// 
