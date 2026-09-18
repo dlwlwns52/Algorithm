76f7c69def17adc41824525d2f3246161316b73b
ezJun <30545469+dlwlwns52@users.noreply.github.com>
2026-09-18 19:05:00 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 0.44 ms, Memory: 4.57 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/README.md
+++ 프로그래머스/2/1844. 게임 맵 최단거리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 게임 맵 최단거리 - 1844 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844?language=cpp) 
 
 ### 성능 요약
 
-메모리: 4.59 MB, 시간: 0.41 ms
+메모리: 4.57 MB, 시간: 0.44 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 09월 15일 11:53:49
+2026년 09월 18일 19:04:54
 
 ### 문제 설명
 
--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.cpp
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.cpp
@@ -1,54 +1,51 @@
 #include<vector>
 #include<queue>
-#include<iostream>
 using namespace std;
+
 
 int solution(vector<vector<int> > maps)
 {
     int answer = 0;
+    vector<vector<bool>> visited(maps.size(), vector<bool>(maps[0].size(), false));
     queue<vector<int>> q;
-    q.push({0,0,1});
-    
-    int m = maps[0].size();
-    int n = maps.size();
-    
     vector<int> dx = {0,0,-1,1};
     vector<int> dy = {1,-1,0,0};
     
-    vector<vector<bool>> visited (n, vector(m, false));
-    visited[0][0] = true;
-    
+    q.push({0,0,1});
+    visited[0][0]= true;
     while(!q.empty()){
-        vector<int> cur = q.front();
+        vector<int> f = q.front();
         q.pop();
         
-        
-        if(cur[0] == m-1 && cur[1] == n-1){
-            answer = cur[2];
-            break;
+        if(f[0] == maps[0].size()-1 && f[1] == maps.size()-1){
+            answer = f[2];
         }
+    
         for(int i=0; i<4; i++){
-            int nx = cur[0] + dx[i];
-            int ny = cur[1] + dy[i];
-            if(0 <= nx && nx < m && 0 <= ny && ny < n && visited[ny][nx] == false && maps[ny][nx]== 1){
-                visited[ny][nx] = true;
-                int cnt = cur[2]+1;
-                q.push({nx,ny, cnt});
+            int nx = f[0] + dx[i];
+            int ny = f[1] + dy[i];
+            if(0<=nx && nx < maps[0].size() && 0 <= ny && ny < maps.size() && visited[ny][nx] == false && maps[ny][nx] == 1){
+              visited[ny][nx]= true;
+                q.push({nx,ny,f[2]+1});
             }
         }
-        
+
     }
-    
-    if(answer == 0){
+        
+    if(answer ==0){
         return -1;
     }
-    
     return answer;
 }
 
 
-// bfs
-// 큐(x,y,거리(1))
-// 큐front값체크 후pop
-// 만약 x 하고 y가 maps[0].size-1 maps.size-1 에도달하면 거리값 달고 반환
-// while문에서는 동서남북중에서 크기 안 넘어가고 1인경우 push
+
+// 큐선언(x,y,count), visited생성, 동서남북전역변수
+// 1. 큐에 초기값을 넣는다 push
+// 2. while(큐가 안비어있을때까지)
+//  - front값 확인
+//  - 큐값 pop
+//  - 현재 front x,y 값이 maps 사이즈 -1이라면 count값 answer에 넣기
+//  - 현재 방문처리
+//  - for 동서남북 , map안에있고 방문안했고 1이라면 push(nx,ny,count+1)
+// 3. answer=0이면 -1반환 
