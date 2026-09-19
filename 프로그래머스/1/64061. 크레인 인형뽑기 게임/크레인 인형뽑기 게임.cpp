28fd89d5e3eafc255b25ea16b0b2ed5b876626fd
ezJun <30545469+dlwlwns52@users.noreply.github.com>
2026-09-19 11:13:47 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 0.02 ms, Memory: 4.92 MB -BaekjoonHub

--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.cpp
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.cpp
@@ -1,50 +1,48 @@
 #include <string>
 #include <vector>
 #include <stack>
+#include <iostream>
 using namespace std;
 
 int solution(vector<vector<int>> board, vector<int> moves) {
     int answer = 0;
     stack<int> st;
     
+
     for(int i=0; i<moves.size(); i++){
         for(int j=0; j<board.size(); j++){
             if(board[j][moves[i]-1] != 0) {
-                if(st.empty() || st.top() != board[j][moves[i]-1]){
-                    st.push(board[j][moves[i]-1]);
-                } 
-                else{
+                if(st.empty() == false && st.top() == board[j][moves[i]-1]){
+                    answer+=2;
                     st.pop();
-                    answer +=2;
+                } else{
+                    st.push(board[j][moves[i]-1]);          
                 }
-                board[j][moves[i]-1] = 0;
+                board[j][moves[i]-1] =0;    
                 break;
             }
         }
     }
+    
     return answer;
 }
 
+// 정사각 - 인형이 없는곳 아무일 x -  사라진 인형의 개수 - 
 
-
-
-// 0. #include<stack> stack<int> 정의
-// 1. moves 길이만큼 for문 진행 i
-// 2. board 행길이만큼 for 문 진행 - 이중for문 j
-// 3. 만약 board[j][moves[i]-1] 가 0이 아니라면
-//  - st이 비어있거나 top() 값이 다르면 push
-//  - st가 비어있지 않고 top() 값이 같으면 st.pop()후에 answer+1 
-//  - board[j][moves[i]-1] 값 0
-//  -break
-
-
-// 
 
 // [[0,0,0,0,0],
 // [0,0,1,0,3],
 // [0,2,5,0,1],
 // [4,2,4,4,2],
-// [3,5,1,3,1]]
+// [3,5,1,3,1]]	
 
-// 04240103
-// 431132''4
+
+// 스택하나 만들기
+// moves길이만큼 for문
+// board 행 길이만큼 for문
+//  - 만약  board[반복][move-1]!=0 이라면
+//     - 만약 스택이 안 비어있고 top이 지금값과 같다면 answer+=2 후 pop
+//     - else라면 push
+//     - board[반복][move-1] = 0
+//     - break
+// 
