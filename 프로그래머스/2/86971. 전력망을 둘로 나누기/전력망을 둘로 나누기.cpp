04f19ab2437f983bb0fcc9c7fcb4d586152c530d
ezJun <30545469+dlwlwns52@users.noreply.github.com>
2026-09-19 16:15:21 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 0.23 ms, Memory: 4.7 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.cpp
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.cpp
@@ -1,70 +1,72 @@
 #include <string>
 #include <vector>
+#include <queue>
 #include <cmath>
-#include <algorithm>
-#include <iostream>
 using namespace std;
-int dfs(int node, vector<int>& cut_nodes, vector<bool> visited, vector<vector<int>>&  graph){
+
+
+int dfs(int node, vector<int> wire, vector<vector<int>>& graph, vector<bool>& visited){
     int count = 1;
-    visited[node] = true;
-    
-    for(int next : graph[node]){
-        if((next == cut_nodes[0] && node == cut_nodes[1]) || (next == cut_nodes[1] && node == cut_nodes[0])){
+    for(int i: graph[node]){
+        if(visited[i] != false){
             continue;
         }
-        if(!visited[next]){
-            count += dfs(next, cut_nodes, visited, graph);
+        if((node == wire[0] && i == wire[1] )||(node == wire[1] && i == wire[0])){
+            continue;
         }
+        visited[i] = true;
+        count += dfs(i, wire, graph, visited);
     }
     return count;
 }
 
+
 int solution(int n, vector<vector<int>> wires) {
     int answer = -1;
+    // 그래프만들기
     vector<vector<int>> graph(n+1);
-    vector<int> node_count;
     
-    for(vector<int> wire : wires){
-        graph[wire[0]].push_back(wire[1]);
-        graph[wire[1]].push_back(wire[0]);
+    for(int i=0; i<wires.size(); i++){
+        graph[wires[i][0]].push_back(wires[i][1]);
+        graph[wires[i][1]].push_back(wires[i][0]);
     }
-    
-    for(vector<int> wire :wires){
-        int count_a = 0;
-        vector<bool> visited(n+1,false);
-        count_a = dfs(wire[0], wire, visited, graph);
-        int count_b = abs(n-count_a);
-        node_count.push_back(abs(count_a-count_b));
-    }
-    
 
+    // priority_queue 
+    priority_queue<int, vector<int>, greater<int>> pq;
     
-    answer = *min_element(node_count.begin(), node_count.end());
-    
+    for(int i=0; i<wires.size(); i++){
+        // visited
+        vector<bool> visited(n+1, false);
+        visited[wires[i][0]] = true;
+        int count_a = dfs(wires[i][0], wires[i], graph, visited);
+        int count_b = abs(n-count_a);
+        int total = abs(count_a-count_b);
+        pq.push(total);
+        
+    }
+    answer = pq.top();
     return answer;
 }
 
 
-// #include <cmath>
-// vector<vector<int>> graph(n+1) 으로 인접리스트 뼈대 생성
-// wires for문,  인접리스트 값 넣기  
+// 인접리스트 만들기 - 그래프 / visited 만들기 n개수대로/ prirority_queue 최소힙
+// 1. wires 개수대로 for문
+//  - int dfs(구해올 노드, 첫번째 노드두번째 노드 벡터, 그래프, visited)
+//  - count_a 값으로 구해올 노드 값 받기
 
-// wires길이만큼 for문  - i
-//   -count_a = 0 , visited 는 n+1만큼
-//   - int dfs(현재 노드[0], 첫번재점두번재점  ,visited, 그래프)
-//     - int count=1;
-//      - visited[노드] = true
-//      -  그래프 for문 - graph[노드] - next
-//      - 만약 
-//             next node가  첫번재 두번째와 같거나 두번째 첫번째점과 같다면 컨티뉴
-//      -  만약 방문하지 않았다면
-//      -  count += dfs
-//     
-//  count_a 반환
 
-// count_b =  abs(count_a - n)
-// 두개 차 벡터에 넣음 ->오름차순에서 첫번째거 출력
- 
-// 1-3
-// 2-3
-// 3-1, 2,4
+//  - dfs
+//  - 현재 노드 개수 count =1
+
+
+//  - 그래프[현재노드] for문해서
+//  
+// - 방문한적이 없다면
+// - 구해올노드 ==첫번째노드, 그래프[노드] == 두번째노드 or  구해올노드 ==두번째노드, 그래프[노드] == 첫번째노드 라면 continue
+//  - count = dfs()
+// - 방문처리
+// - 마지막에 count반환 
+
+// 2. 받아왔으면 abs(n-count_a)로 두번째 노드값 구하기
+// 3. 차를 prirority_queue에 넣음
+// 4. 다했으면 미니힙 top확인
