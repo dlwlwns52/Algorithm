03e6f4aabc8016e1bb771eab254cf61d2fb6ee7f
ezJun <30545469+dlwlwns52@users.noreply.github.com>
2026-09-21 13:49:00 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 0.02 ms, Memory: 4.82 MB -BaekjoonHub

--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.cpp
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.cpp
@@ -1,48 +1,39 @@
 #include <string>
 #include <vector>
 #include <stack>
-#include <iostream>
+
 using namespace std;
 
 int solution(vector<vector<int>> board, vector<int> moves) {
     int answer = 0;
     stack<int> st;
-    
-
-    for(int i=0; i<moves.size(); i++){
+    for (int i=0; i<moves.size(); i++){
         for(int j=0; j<board.size(); j++){
-            if(board[j][moves[i]-1] != 0) {
-                if(st.empty() == false && st.top() == board[j][moves[i]-1]){
+            if(board[j][moves[i]-1] == 0){
+                continue;
+            }
+            else{
+                if(!st.empty() && st.top() == board[j][moves[i]-1]){
                     answer+=2;
                     st.pop();
+                    board[j][moves[i]-1]= 0;
+                    break;
                 } else{
-                    st.push(board[j][moves[i]-1]);          
+                    st.push(board[j][moves[i]-1]);
+                    board[j][moves[i]-1]= 0;
+                    break;
                 }
-                board[j][moves[i]-1] =0;    
-                break;
             }
         }
     }
-    
     return answer;
 }
 
-// 정사각 - 인형이 없는곳 아무일 x -  사라진 인형의 개수 - 
 
+// 스택생성
 
-// [[0,0,0,0,0],
-// [0,0,1,0,3],
-// [0,2,5,0,1],
-// [4,2,4,4,2],
-// [3,5,1,3,1]]	
-
-
-// 스택하나 만들기
-// moves길이만큼 for문
-// board 행 길이만큼 for문
-//  - 만약  board[반복][move-1]!=0 이라면
-//     - 만약 스택이 안 비어있고 top이 지금값과 같다면 answer+=2 후 pop
-//     - else라면 push
-//     - board[반복][move-1] = 0
-//     - break
-// 
+// moves길이대로 for문
+//  for문 보드 행길이 j
+// 만약 board[j][moves-1]가  0이라면 컨티뉴
+// 숫자라면 만약 스택이 존재하고 값이 같다면 pop answer+=2, 0처리, break
+//  값이 틀리다면 push
