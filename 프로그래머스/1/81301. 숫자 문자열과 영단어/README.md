bce69c150b9a8a15b469e720cc0572489d981004
ezJun <30545469+dlwlwns52@users.noreply.github.com>
2026-09-21 20:33:54 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.01 ms, Memory: 4.63 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/README.md
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/README.md
@@ -1,10 +1,10 @@
 # [level 1] 숫자 문자열과 영단어 - 81301 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/81301) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/81301?language=cpp) 
 
 ### 성능 요약
 
-메모리: 4.95 MB, 시간: 0.01 ms
+메모리: 4.63 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 09월 10일 20:42:37
+2026년 09월 21일 20:33:48
 
 ### 문제 설명
 
--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.cpp
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.cpp
@@ -1,41 +1,40 @@
 #include <string>
 #include <vector>
-#include <iostream>
 #include <algorithm>
+#include <iostream>
 using namespace std;
 
 int solution(string s) {
     int answer = 0;
-    string toint;
-    string temp;
-    vector<string> voka = {"zero","one","two","three","four","five","six","seven","eight","nine"};
-
-    for (char c : s){
-      
-        if (isdigit(c)){
+    string total, temp;
+    vector<string> sv = {"zero","one", "two", "three", "four","five","six","seven","eight","nine"};
+    vector<int> si = {0,1,2,3,4,5,6,7,8,9};
+    
+    for(char c : s){
+        if(find(si.begin(), si.end(), c-'0') == si.end()){
             temp += c;
-            continue;
-        }
-        toint += c;
-        if (find(voka.begin(), voka.end(), toint) != voka.end()) {
-            auto it = find(voka.begin(), voka.end(), toint);
-            int index = it - voka.begin();
-            temp += to_string(index);
-            toint = "";
+            if(find(sv.begin(), sv.end(), temp) != sv.end()){
+                int iv = find(sv.begin(), sv.end(), temp) - sv.begin();
+                total += to_string(si[iv]);
+                temp = ""; // 초기화 이렇게하는게 맞나
+            }
+        } else{
+            total += c;
         }
     }
-    // cout << temp;
-    answer = stoi(temp);
+    answer = stoi(total);
+    
     return answer;
 }
 
 
-// 1. 2개의 벡터 사용? 숫자, 영단어 보관용 -> map은 현재 미사용 /
-// 2. 하나 문자열 합칠때마다 비교?  
 
-
-
-// 벡터에서 인덱스를 어떻게뽑지? find -> it - begin() / find쓸거면 필수
-// '3'이게 숫자다 라는걸 어덯게 알지? -> 지금은 아스키로가자 => 아스키'2' 가 50이다.. -> isdigit()
-// '3'을 실제 숫자 3으로 바꾸려면 : c - '0'
-// 벡터에서 if(find) 가능하나
+// 맵 -> 키숫자, 영단어벨류
+// string total, string temp
+// 벡터 : 문자열벡터 숫자벡터
+// for 문 s길이만큼
+//  - 만약 지금 있는 값이 숫자벡터에 없는거라면
+//      - 지금값 temp += 's'
+//      -  만약 temp가 문자열 벡터에있다면
+//      - 인덱스 가져와서 숫자인덱스에 대입해서 값가져와서 문자로 total+=
+//  - 그냥 숫자 문자로 total +=
